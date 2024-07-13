cc170537b549623ff0ce2870fbc307556df95de8
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-07-13 16:27:19 +0200

    update with blockscout contract verification

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 source .env
 
 # To deploy and verify our contract
-forge script --chain sepolia script/VeNftDiscountHook.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
+# etherscan
+#forge script --chain sepolia script/VeNftDiscountHook.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
+
+# blockscout
+forge script --chain sepolia script/VeNftDiscountHook.s.sol:MyScript --verifier=blockscout --verifier-url=https://eth-sepolia.blockscout.com/api/ --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
